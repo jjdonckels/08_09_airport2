185ab6b516859314f10269eaa0d584dd84f174a9
James Donckels <jdonckels1@go.pasadena.edu>
2024-02-22 10:14:18 -0800

    implemented _print_list

---
+++ includes/linked_list_functions/linkedlistfunctionsTemplated.h
@@ -0,0 +1,75 @@
+#ifndef LINKEDLISTFUNCTIONSTEMPLATED_H
+#define LINKEDLISTFUNCTIONSTEMPLATED_H
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <assert.h>
+#include "../node/node.h"
+
+//Linked List General Functions:
+template <typename T>
+void _print_list(node<T>* head){
+    node<T>* temp;
+    temp = head;
+    assert(temp != NULL);
+    while (temp != NULL){
+        cout << *temp;
+        temp = temp->_next;
+    }
+    cout << "NULL";
+}
+
+// //recursive fun! :)
+// template <typename T>
+// void _print_list_backwards(node<T> *head);
+
+// //return ptr to key or NULL
+// template <typename T>
+// node<T>* _search_list(node<T>* head,
+//                             T key);
+
+
+// template <typename T>
+// node<T>* _insert_head(node<T> *&head,
+//                             T insert_this);
+
+// //insert after ptr: insert head if marker null
+// template <typename T>
+// node<T>* _insert_after(node<T>*& head,
+//                                 node<T> *after_this,
+//                                 T insert_this);
+
+// //insert before ptr: insert head if marker null
+// template <typename T>
+// node<T>* _insert_before(node<T>*& head,
+//                                 node<T>* before_this,
+//                                 T insert_this);
+
+// //ptr to previous node
+// template <typename T>
+// node<T>* _previous_node(node<T>* prev_to_this);
+
+// //delete, return item
+// template <typename T>
+// T _delete_node(node<T>*& head, node<T>* delete_this);
+
+// //duplicate the list...
+// template <typename T>
+// node<T>* _copy_list(node<T>* head);
+
+// //duplicate the list, return pointer to last node in dest... 
+// //     use this function in your queue big three 
+// template <typename T>
+// node<T> *_copy_list(node<T> *&dest, node<T> *src);
+
+
+
+// //delete all the nodes
+// template <typename T>
+// void _clear_list(node<T>*& head);
+
+// //_item at this position
+// template <typename T>
+// T& _at(node<T>* head, int pos);
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <iomanip>
+#include "includes/linked_list_functions/linkedlistfunctionsTemplated.h"
+#include "includes/node/node.h"
 
 
 using namespace std;
 
 int main(int argv, char** argc) {
-    cout << "\n\n"
-         << endl;
+    // cout << "\n\n"
+    //      << endl;
 
-    cout << "\n\n\n=====================" << endl;
+    // cout << "\n\n\n=====================" << endl;
+
+    // test linked list
+    node<int> d(20);
+    node<int> c(15, &d, NULL);
+    node<int> b(10, &c, NULL);
+    node<int> a(5, &b, NULL);
+
+    node<int>* head = &a;
+
+    cout << "Testing _print_list:\n";
+
+    _print_list(head);
+
+    cout << endl;
+
     return 0;
 }
